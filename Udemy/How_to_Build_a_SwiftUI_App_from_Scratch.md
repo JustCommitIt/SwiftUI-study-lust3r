6bbe8a62e4ae15694d936c7776c94ba4a24153ac
Lust3r <s1skyblue1s@gmail.com>
2023-04-24 21:42:08 +0900

    [Feat] I Am Rich 프로젝트 추가

---
+++ Udemy/How_to_Build_a_SwiftUI_App_from_Scratch.md
@@ -0,0 +1,8 @@
+# 학습 목표
+
+1. How to start your first SwiftUI Project
+2. Work with Xcode preview and the object library to generate SwiftUI code
+3. Learn to use SwiftUI modifiers to set properties for components
+4. Learn how to arrange elements using SwiftUI Stacks
+5. Learn how to add and size Image components
+
---
+++ Udemy/I Am Rich/I Am Rich/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by 이상윤 on 2023/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
